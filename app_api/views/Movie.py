7c522861f46bffc71c2d1b5a34a5f1b92f607609
Devin <dalamcd@gmail.com>
2022-05-25 10:36:38 -0500

    First session

--- app_api/views/Movie.py
+++ app_api/views/Movie.py
@@ -1,12 +1,22 @@
 from django.http import HttpResponseServerError
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
+from app_api.models import Movie
+from app_api.models import Genre
+from rest_framework import serializers
 
 class MovieView(ViewSet):
 
     def list(self, request):
-        pass
-
+        movies = Movie.objects.all()
+        genre = self.request.query_params.get('genre', None)
+        if genre is not None:
+            movies = movies.filter(genre=genre)
+            for movie in movies:
+                genre = Genre.objects.get(pk=movie.genre.id)
+                movie.genre = genre
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
     def create(self, request):
         pass
 
@@ -15,3 +25,17 @@ class MovieView(ViewSet):
 
     def destroy(self, request, pk):
         pass
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = ('id', 'name')
+    
+class MovieSerializer(serializers.ModelSerializer):
+
+        class Meta:
+            model = Movie
+            fields = ('id', 'title', 'description', 'run_time', 'user', 'date_released', 'genre')
+            # depth = 1
