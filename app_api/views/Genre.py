6639479bbff1c5f6e443ae8de2f73debf0122939
Devin <dalamcd@gmail.com>
2022-05-25 14:36:32 -0500

    Initial session

--- app_api/views/Genre.py
+++ app_api/views/Genre.py
@@ -1,13 +1,18 @@
 from django.http import HttpResponseServerError
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
-
+from rest_framework import serializers, status
+from app_api.models.Genre import Genre
+from rest_framework import serializers
 
 class GenreView(ViewSet):
     """View for Genres"""
 
     def list(self, request):
-        pass
+        """"Handle get request for Genres"""
+        genres = Genre.objects.all()
+        serializer = GenreSerializer(genres, many=True)
+        return Response(serializer.data)
 
     def create(self, request):
         pass
@@ -17,3 +22,10 @@ class GenreView(ViewSet):
 
     def destroy(self, request, pk):
         pass
+
+class GenreSerializer(serializers.ModelSerializer):
+    """JSON serializerserializers"""
+    class Meta:
+     
+        model = Genre
+        Fields =Genre
--- app_api/views/Movie.py
+++ app_api/views/Movie.py
@@ -1,17 +1,39 @@
 from django.http import HttpResponseServerError
+from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
+from app_api.models.Movie import Movie
 
 class MovieView(ViewSet):
 
     def list(self, request):
-        pass
+        movies = Movie.objects.all()
+        user = request.auth.user
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
 
-    def create(self, request):
+        
+    def create(self,request):
         pass
+        # foreign keys
+        # user = request.auth.user
+        # genre = Genre.objects.get(pk=request.data['genre'])
+        # serializer = CreateMovieSerializer
 
     def update(self, request, pk):
         pass
 
     def destroy(self, request, pk):
-        pass
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Movie
+        fields = (
+            'id', 'title', 'description', 'run_time', 'date_released', 'genre'
+        )
+        depth = 1    
