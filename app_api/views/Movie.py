7e83fa013e6b6b6e2f0ab1188d3f50ed43e78ec6
Devin <dalamcd@gmail.com>
2022-05-26 13:06:09 -0500

    Second session.

--- app_api/views/Movie.py
+++ app_api/views/Movie.py
@@ -3,6 +3,9 @@ from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 from app_api.models.Movie import Movie
+from app_api.models.Genre import Genre
+from django.contrib.auth.models import User
+
 
 class MovieView(ViewSet):
 
@@ -12,13 +15,18 @@ class MovieView(ViewSet):
         serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data)
 
-        
+        # Let's burn this mother to the ground.
     def create(self,request):
-        pass
-        # foreign keys
-        # user = request.auth.user
-        # genre = Genre.objects.get(pk=request.data['genre'])
-        # serializer = CreateMovieSerializer
+        # Access foreign keys
+        # Properties on data sent for POST:
+            # 
+        user = User.objects.get(pk=request.data['user'])
+        genre = Genre.objects.get(pk=request.data['genre'])
+        serializer = CreateMovieSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=user, genre=genre)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
     def update(self, request, pk):
         pass
@@ -34,6 +42,20 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = (
-            'id', 'title', 'description', 'run_time', 'date_released', 'genre'
+            'id', 'title', 'description', 'run_time', 'date_released', 'genre', 'user'
         )
         depth = 1    
+
+class CreateMovieSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Movie
+        fields = (
+            'title',
+            'description',
+            'run_time',
+            'date_released'
+        )
+
+        # Create movies with a user attached that isn't the user making the request. 
+        # Think "suggested movies" on a streaming platform
